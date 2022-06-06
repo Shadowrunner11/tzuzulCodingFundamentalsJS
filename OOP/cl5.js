bb02381d84214859ded8dcce6320c03584cceb06
Shadowrunner11 <matematicadivina1@gmail.com>
2022-06-06 10:11:22 -0500

    [update] homwework oop

---
+++ OOP/cl5.js
@@ -0,0 +1,14 @@
+//Abstraccion
+//Encapsulacion
+
+//30 y 30
+//cada metodo debe encargarse de una cosa y una sola
+
+//Especificaciones:
+//UH: Debo ingresar a un menu que me pida mi nombre de usuario
+//con la posibilidad de corregirlo si no estoy
+//conforme
+
+//UH: Debo poder ver un menu de opciones del juego, como 
+//empezar el juego y configurar la dificultad, ademas
+//de poder escoger un nivel arbitrario
